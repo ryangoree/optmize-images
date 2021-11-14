a05c444ce19a5ab49d78faced317aff222d70b80
Ryan Goree <goree.ryan@gmail.com>
2021-11-14 04:49:14 -0600

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const path = require('path')
+const jimp = require('jimp')
+
+const inputDirectoryPath = path.join(__dirname, './input')
+const outputDirectoryPath = path.join(__dirname, './output')
+const processedDirecoryPath = path.join(__dirname, './processed')
+
+async function run() {
+  const files = await fs.promises.readdir(inputDirectoryPath)
+  await Promise.all(
+    files
+      .filter((file) => /^org-portrait--/.test(file))
+      .map(async (file) => {
+        console.log(file)
+        const inputImagePath = path.join(inputDirectoryPath, file)
+        const outputImagePath = path.join(outputDirectoryPath, file)
+        const processedImagePath = path.join(processedDirecoryPath, file)
+        const image = await jimp.read(inputImagePath)
+        await image.cover(160, 160)
+        await image.quality(60)
+        await image.writeAsync(outputImagePath)
+        await fs.promises.rename(inputImagePath, processedImagePath)
+      })
+  )
+}
+
+run()
